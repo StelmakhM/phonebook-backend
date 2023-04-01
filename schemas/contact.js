83980c614909c0acb0f21a5d9f0213eaa22da4a3
Maksym Stelmakh <stelmakh1994@gmail.com>
2023-04-01 10:06:15 +0300

    validation schema fix

--- schemas/contact.js
+++ schemas/contact.js
@@ -2,9 +2,9 @@ const Joi = require("joi");
 
 const addContactSchema = Joi.object({
 	name: Joi.string().required(),
-	email: Joi.string(),
-	phone: Joi.number(),
-	address: Joi.string(),
+	email: Joi.string().empty("").default(null),
+	phone: Joi.number().empty("").default(null),
+	address: Joi.string().empty("").default(null),
 	favorite: Joi.bool(),
 });
 
